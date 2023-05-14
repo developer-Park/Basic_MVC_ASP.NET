84cc7e9931dbaf2f2a00b0dbcf42e4ebc3a96ecb
developer-Park <kyle911011@gmail.com>
2023-05-14 14:19:10 +0900

    초기 커밋.

---
+++ eCommerce/Models/AppUser.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace eCommerce.Models
+{
+    public class AppUser
+    {
+        public int? Pace { get; set; }
+
+        public int? Mileage { get; set; }
+        public Address? Address { get; set; }
+        public ICollection<Club> Clubs { get; set; }
+        public ICollection<Race> Races{ get; set; }
+
+
+
+
+    }
+}
